8108b04a3cabc96bfff36bd37fbeaf0f831ff3cd
Ramanuja <ramatman+infinite-happiness@gmail.com>
2024-09-14 21:56:48 +0200

    ADD sitemap w script

---
+++ sitemap.py
@@ -0,0 +1,37 @@
+import os
+import datetime
+from xml.etree import ElementTree as ET
+
+
+def generate_sitemap(base_url, files):
+    """Generates a sitemap.xml file from HTML files in a given directory.
+
+    Args:
+      directory: The directory to scan for HTML files.
+    """
+
+    sitemap = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+
+    for file in files:
+        if file == "index.html":
+            url = base_url
+        else:
+            url = base_url + file
+
+        last_modified = datetime.datetime.fromtimestamp(
+            os.path.getmtime(file)
+        ).strftime("%Y-%m-%dT%H:%M:%SZ")
+        url_element = ET.SubElement(sitemap, "url")
+        ET.SubElement(url_element, "loc").text = url
+        ET.SubElement(url_element, "lastmod").text = last_modified
+        # Add other elements like <changefreq> and <priority> if needed
+
+    tree = ET.ElementTree(sitemap)
+    tree.write("sitemap.xml", encoding="utf-8", xml_declaration=True)
+
+
+# Replace 'your_directory' with the actual directory path
+generate_sitemap(
+    "https://infinite-happiness.github.io/Ananda-Marga-Fasting-Calendar/",
+    ["index.html", "science.html"],
+)
